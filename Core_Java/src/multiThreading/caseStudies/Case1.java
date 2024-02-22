2a774c94e09cd611bc4c12167643d8c4a71002d3
AkashShetty2001 <akashkd9911@gmail.com>
2024-02-22 22:02:00 +0530

    case studies for multiThreading explanation

---
+++ Core_Java/src/multiThreading/caseStudies/Case1.java
@@ -0,0 +1,23 @@
+package multiThreading.caseStudies;
+
+class MyRunnabale implements Runnable{
+	@Override
+	public void run() {
+		System.out.println("Child Thread");
+		
+	}
+}
+public class Case1 {
+	public static void main(String[] args) {
+		MyRunnabale r= new MyRunnabale();
+		
+		Thread t = new Thread(r);
+		//Thread t1 = new Thread(r);
+		
+		t.start();// making a call to Thread class run() , there are currently 2 Threads.
+		
+		System.out.println("Main Thread");
+		
+	}
+
+}
