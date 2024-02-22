7ca2c607b6dba798594d1163d7f8e380064dbb03
AkashShetty2001 <akashkd9911@gmail.com>
2024-02-22 22:07:07 +0530

    added comments

--- Core_Java/src/multiThreading/caseStudies/Case1.java
+++ Core_Java/src/multiThreading/caseStudies/Case1.java
@@ -11,8 +11,8 @@ public class Case1 {
 	public static void main(String[] args) {
 		MyRunnabale r= new MyRunnabale();
 		
-		Thread t = new Thread(r);
-		//Thread t1 = new Thread(r);
+		Thread t = new Thread();
+		Thread t1 = new Thread(r);
 		
 		t.start();// making a call to Thread class run() , there are currently 2 Threads.
 		
