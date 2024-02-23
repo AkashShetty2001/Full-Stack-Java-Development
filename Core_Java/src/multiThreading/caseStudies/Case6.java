a9ac3e63eb71d33e75d636a5f998ac5d573edf18
AkashShetty2001 <akashkd9911@gmail.com>
2024-02-23 22:31:32 +0530

    6th case study

---
+++ Core_Java/src/multiThreading/caseStudies/Case6.java
@@ -0,0 +1,28 @@
+package multiThreading.caseStudies;
+
+class MyRunnabale6 implements Runnable{
+	@Override
+	public void run() {
+		System.out.println("Child Thread");
+		
+	}
+}
+public class Case6 {
+	public static void main(String[] args) {
+		MyRunnabale6 r= new MyRunnabale6();
+		
+		Thread t = new Thread();
+		Thread t1 = new Thread(r);
+		
+		r.run();//
+		/*no  new  thread , my using r MyRunnable class run() is called. */
+		
+		System.out.println("Main Thread");
+		
+	}
+
+	
+	
+	
+	
+}
