e08a56a28fdb1585937b2fcd44bd1a82b28962ad
AkashShetty2001 <akashkd9911@gmail.com>
2024-07-07 00:52:18 +0530

    example programs on abstraction concept in oops

---
+++ Core_Java/src/oopsConcepts/abstraction/AbstractionEx1.java
@@ -0,0 +1,18 @@
+package oopsConcepts.abstraction;
+
+public class AbstractionEx1 {
+	public static void main(String args[]) {
+		
+	}
+
+}
+
+abstract class Animal{
+	public Animal() {
+		
+	}
+}
+
+class Dog extends Animal{
+	
+}
